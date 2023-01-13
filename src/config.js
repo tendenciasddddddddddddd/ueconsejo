6ae8c3e86a5b74e23e9c109d4c46d5b2e3f416a9
tendenciasddddddddddddd <10004095632w@gmail.com>
2023-01-12 19:56:44 -0500

    first commit

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ config();
 
 //mongodb+srv://steban:Medid100.@serverlessinstance0.0l8ym.mongodb.net/uehuaca?retryWrites=true&w=majority
 export default {
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://steban:Medid100.@serverlessinstance0.0l8ym.mongodb.net/uehuaca?retryWrites=true&w=majority",
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost/sistema-educativo",
   PORT: process.env.PORT || 4000,
   SECRET: 'unidad-educativa-huaca'
 };
